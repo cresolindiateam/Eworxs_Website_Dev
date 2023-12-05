fccc84381b11a9dbc11bd7f1fb0c634a64653c6a
cresolindiateam <nitesh@cresol.in>
2023-12-05 00:28:29 -0800

    update

--- dbconfig.php
+++ dbconfig.php
@@ -1,17 +1,17 @@
 <?php
-function db_connect()
-{
-    $server = 'localhost'; // this may be an ip address instead
-    $user = 'eworxs';
-    $pass = 'Dgoud123!@#';
-    $database = 'EworxsDB'; // name of your database
+
+function db_connect(){
+  $server = 'localhost'; // this may be an ip address instead
+  $user = 'deveworxs';
+  $pass = 'nx]~0-(BGJ{^';
+  $database = 'EworxsDevDB'; // name of your database
       // Create connection
     $conn= mysqli_connect($server,$user,$pass,$database);
-    if(!$conn)
-    {
+    
+    return $conn;
+    if(!$conn){
        die("Connection failed: " . mysqli_connect_error());
     }
-    return $conn;
 }
 ?>
 
