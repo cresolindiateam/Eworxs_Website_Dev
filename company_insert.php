c1ac99af9a6aba49de99347834d1927404c66a70
cresolindiateam <nitesh@cresol.in>
2023-12-05 00:43:02 -0800

    update

--- company_insert.php
+++ company_insert.php
@@ -7,7 +7,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require 'dbconfig.php';
+require 'devwebsite/dbconfig.php';
 
 $error = [];
 $success = [];
